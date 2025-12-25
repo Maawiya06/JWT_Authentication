2579c5f011fb87b7d99352dd7f2d432f2af63b27
Maawiya06 <maawiyaameer001@gmail.com>
2025-12-25 14:56:03 +0530

    commit

--- src/main/java/com/jwt/Controller/Home.java
+++ src/main/java/com/jwt/Controller/Home.java
@@ -1,4 +1,15 @@
 package com.jwt.Controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class Home {
+
+    @RequestMapping("/welcome")
+    public String Welcome(){
+        String text = "this is private page";
+        text += "this page is not allowed to unauthenticated users";
+        return text;
+    }
 }
