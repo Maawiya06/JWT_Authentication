281e9c3c3cd91cd112b9a6bbc45814729347bd7f
Maawiya06 <maawiyaameer001@gmail.com>
2025-12-26 22:04:25 +0530

    commit

--- src/main/java/com/jwt/security/JWTHelper.java
+++ src/main/java/com/jwt/security/JWTHelper.java
@@ -1,9 +1,33 @@
 package com.jwt.security;
 
+import io.jsonwebtoken.Claims;
+import java.util.Date;
+import java.util.function.Function;
+
 import org.springframework.stereotype.Component;
 
 @Component
 public class JWTHelper {
+
+    // required time
+    private static final long JWT_TOKEN_VALIDITY = 5 * 60 * 60;
+
+    public String secret = "afafasfafafasfasfasfafacasdasfasxASFACASDFACASDFASFASFDAFASFASDAADSCSDFADCVSGCFVADXCcadwavfsfarvf";
+
+    // retrieve username form JWT Toke
+    public String getUserNameFromToken(String token){
+        return getClaimFromToken(token, Claims::getSubject);
+    }
+
+    // retrieve Expiration data from jwt token
+    public Date getExpirationDateFromToken(String token){
+        return getClaimFromToken(token, Claims::getExpiration);
+    }
+
+    public <T> T getClaimFromToken(String token, Function<Claims, T> claimsResolver){
+        final Claims claims = getAllClaimsFromToken(token);
+        return claimsResolver.apply(claims);
+    }
 
 
 }
