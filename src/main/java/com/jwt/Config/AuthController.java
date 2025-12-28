5cb3e8b740548d53efd3193c0fc170cc563239f3
Maawiya06 <maawiyaameer001@gmail.com>
2025-12-29 01:17:28 +0530

    commit

--- src/main/java/com/jwt/Config/AuthController.java
+++ src/main/java/com/jwt/Config/AuthController.java
@@ -5,10 +5,13 @@ import com.jwt.Model.JWTResponse;
 import com.jwt.security.JWTHelper;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -35,7 +38,21 @@ public class AuthController {
         UserDetails userDetails = userDetailsService.loadUserByUsername(request.getEmail());
         String token = this.helper.generateToken(userDetails);
 
-        JWTResponse response = JWTResponse.builder().
+        JWTResponse response = JWTResponse
+                               .builder()
+                               .jwtToken(token)
+                               .username(userDetails.getUsername())
+                               .build();
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+    public void doAuthenticate(String email, String password){
+        UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(email, password);
+        try{
+            manager.authenticate(authentication);
+        }catch(BadCredentialsException e){
+            throw new RuntimeException("Invalid UserName and Password !!");
+        }
     }
 
 
