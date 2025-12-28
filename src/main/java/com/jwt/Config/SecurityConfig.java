af0ad5044ba76fd2c95541d4ec29d30a9db45c87
Maawiya06 <maawiyaameer001@gmail.com>
2025-12-28 21:48:00 +0530

    commit

--- src/main/java/com/jwt/Config/SecurityConfig.java
+++ src/main/java/com/jwt/Config/SecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 public class SecurityConfig {
@@ -28,6 +29,8 @@ public class SecurityConfig {
                         .authenticated())
                 .exceptionHandling(ex-> ex.authenticationEntryPoint(point))
                 .sessionManagement(session-> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+        httpSecurity.addFilterBefore(filter, UsernamePasswordAuthenticationFilter.class);
 
         return httpSecurity.build();
     }
