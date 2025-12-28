4fc38f17e6e0fb867383e24a54b2eaf9549be19c
Maawiya06 <maawiyaameer001@gmail.com>
2025-12-28 13:00:14 +0530

    commit

---
+++ src/main/java/com/jwt/security/JWTAuthenticationFilter.java
@@ -0,0 +1,35 @@
+package com.jwt.security;
+
+import com.jwt.Service.UserService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class JWTAuthenticationFilter extends OncePerRequestFilter {
+
+    private Logger logger = LoggerFactory.getLogger(OncePerRequestFilter.class);
+
+    @Autowired
+    private JWTHelper jwtHelper;
+    @Autowired
+    private UserService userService;
+
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain) throws ServletException,
+                                    IOException {
+
+
+    }
+}
